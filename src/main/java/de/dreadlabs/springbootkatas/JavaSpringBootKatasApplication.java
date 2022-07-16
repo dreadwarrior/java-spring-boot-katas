2fe5485aabf1023284355096552278647ccdffc4
Thomas Juhnke <tommy@van-tomas.de>
2022-07-16 15:30:26 +0200

    Initial commit of Spring Boot application
    This project serves a Kata-like playground for testing and fiddling
    around with some Spring Boot features. For starting purposes, it comes
    with the following features installed:
    
    - Actuator
    - Cache
    - HATEOAS
    - Quartz
    - Security4
    - WebMVC
    - DevTools
    - Testcontainers

---
+++ src/main/java/de/dreadlabs/springbootkatas/JavaSpringBootKatasApplication.java
@@ -0,0 +1,13 @@
+package de.dreadlabs.springbootkatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootKatasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootKatasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dreadlabs/springbootkatas/JavaSpringBootKatasApplicationTests.java
@@ -0,0 +1,14 @@
+package de.dreadlabs.springbootkatas;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootKatasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
