4a4b8dc0facdc9b4f1dd4fa83154c85dd6f58d60
Thomas Juhnke <tommy@van-tomas.de>
2022-07-16 15:31:31 +0200

    Rename application class

--- src/main/java/de/dreadlabs/springbootkatas/Application.java
+++ src/main/java/de/dreadlabs/springbootkatas/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaSpringBootKatasApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaSpringBootKatasApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
